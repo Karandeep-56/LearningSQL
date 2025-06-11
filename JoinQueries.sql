4894b160a539da3b0667dd07ad4599e4e357d758
Karandeep-56 <karandeep.singh9121@gmail.com>
2025-06-10 22:43:36 -0700

    Inner join query

--- JoinQueries.sql
+++ JoinQueries.sql
@@ -1,7 +1,4 @@
-SELECT * FROM monarchs
-
-____ 
-SELECT * FROM presidents AS p1 INNER JOIN prime_ministers AS p2 
-USING (country);
-
-
+SELECT prime_ministers.country, prime_ministers.continent, prime_minister, president 
+FROM presidents 
+INNER JOIN prime_ministers
+ON prime_ministers.country = presidents.country
