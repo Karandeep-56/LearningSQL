bac0b93d4da9c0487ea8457cd9e0aeb7755eeace
Karandeep-56 <karandeep.singh9121@gmail.com>
2025-06-10 22:47:04 -0700

    Inner Join using Aliases

--- JoinQueries.sql
+++ JoinQueries.sql
@@ -2,3 +2,8 @@ SELECT prime_ministers.country, prime_ministers.continent, prime_minister, presi
 FROM presidents 
 INNER JOIN prime_ministers
 ON prime_ministers.country = presidents.country
+
+----Inner join using Aliases 
+SELECT p2.country, p2.continent, prime_minister, president 
+FROM presidents AS p1 INNER JOIN prime_ministers AS p2 
+ON p1.country = p2.country 
