2d235a9d422cda06b3feb0cc8a2378b208596e25
Karandeep-56 <karandeep.singh9121@gmail.com>
2025-06-11 22:09:07 -0700

    Chained inner join with using

--- JoinQueries using multiple keys.sql
+++ JoinQueries using multiple keys.sql
@@ -1,4 +1,4 @@
-SELECT * FROM presidents AS p1 INNER JOIN prime_ministers AS p2 
-ON p1.country = p2.country 
-INNER JOIN states AS p3 
-ON p1.country = p3.country
+SELECT p1.country, p1.continent, president, prime_minister 
+FROM prime_ministers AS p1 INNER JOIN presidents AS p2 
+USING (country)
+INNER JOIN prime_minister_terms AS p3 USING (prime_minister)
