168b0e4e90c5c13c2eb4cf65ccd3664afc51327e
Karandeep-56 <karandeep.singh9121@gmail.com>
2025-06-12 21:35:15 -0700

    Calculating cost by meal_id

--- Foodr Project part 1.sql
+++ Foodr Project part 1.sql
@@ -1,4 +1,17 @@
-SELECT order_id, SUM(meal_cost * order_quantity) AS Revenue 
-FROM meals
-JOIN orders ON meals.meal_id = orders.meal_id
-GROUP BY order_id 
+---Revenue by order_id 
+SELECT
+	ORDER_ID,
+	SUM(MEAL_COST * ORDER_QUANTITY) AS REVENUE
+FROM
+	MEALS
+	JOIN ORDERS ON MEALS.MEAL_ID = ORDERS.MEAL_ID
+GROUP BY
+	ORDER_ID
+
+---- Calculating cost 
+SELECT meals.meal_id, SUM(meal_cost * stocked_quantity ) AS Total_cost 
+From meals 
+JOIN stock ON meals.meal_id = stock.meal_id  
+GROUP BY meals.meal_id
+ORDER BY Total_cost DESC 
+LIMIT 3
