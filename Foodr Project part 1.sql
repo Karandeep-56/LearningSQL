7c71b02f34b2906e4169dc8af1b8c34fde212fca
Karandeep-56 <karandeep.singh9121@gmail.com>
2025-06-12 22:44:40 -0700

    Queries

--- Foodr Project part 1.sql
+++ Foodr Project part 1.sql
@@ -1,3 +1,44 @@
+----Calculating Revenue 
+SELECT  
+  order_id,  
+  SUM(meal_price * order_quantity) AS revenue  
+FROM meals  
+JOIN orders ON meals.meal_id = orders.meal_id  
+GROUP BY order_id;
+
+
+-----Calculating cost 
+SELECT  
+  meals.meal_id,   
+  SUM(meal_cost * stocked_quantity) AS cost   
+FROM meals  
+JOIN stock ON meals.meal_id = stock.meal_id  
+GROUP BY meals.meal_id  
+ORDER BY meals.cost DESC  
+LIMIT 3;
+
+---- Using common table expression (CTE)
+WITH costs_and_quantities AS (  
+ SELECT  
+   meals.meal_id,  
+   SUM(stocked_quantity) AS quantity,  
+   SUM(meal_cost * stocked_quantity) AS cost  
+ FROM meals  
+ JOIN stock ON meals.meal_id = stock.meal_id  
+ GROUP BY meals.meal_id 
+)   
+SELECT   
+ meal_id,  
+ quantity,  
+ cost  
+FROM costs_and_quantities  
+ORDER BY cost DESC  
+LIMIT 3; 
+
+
+
+
+
 ----Calculating profit by bringing revenue and cost together 
 
 With Revenue AS 
