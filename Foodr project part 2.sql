4009d084ce1f659f25b69dcc5d6b1c04b9e60553
Karandeep-56 <karandeep.singh9121@gmail.com>
2025-07-04 11:30:17 -0700

    Revenue bucketing using case

--- Foodr project part 2.sql
+++ Foodr project part 2.sql
@@ -189,6 +189,35 @@ SELECT
 	 GROUP BY meal_category 
 	 order by meal_category
 
+---revenue bucketing using case 
+with user_revenue AS 
+(SELECT user_id, 
+   SUM (meal_price * order_quantity) AS revenue
+   FROM meals JOIN orders ON meals.meal_id = orders.meal_id
+   GROUP BY user_id 
+ )
+   SELECT 
+         CASE 
+		      WHEN revenue< 150 THEN 'low revenue meal'
+			  WHEN revenue < 300 THEN 'mid-revenue meal' 
+			  ELSE 'high revenue meal' END AS revenue_group, 
+			  COUNT(DISTINCT user_id) AS users 
+			  FROM user_revenue
+          GROUP BY revenue_group 
+		  ORDER BY revenue_group 
+		  LIMIT 5;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
